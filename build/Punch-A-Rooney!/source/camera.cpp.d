4fc7afb2f87564f8f3f66e50da1a9455a25b979f
seaners42 <seaners42@gmail.com>
2026-07-31 04:50:52 -0400

    Few tweaks, more comments

--- build/Punch-A-Rooney!/source/camera.cpp.d
+++
@@ -1,3 +0,0 @@
-build/Punch-A-Rooney!/source/camera.cpp.o: source/camera.cpp \
- source/camera.h
-source/camera.h:
--- build/Punch-A-Rooney!/source/usefulfunctions.cpp.d
+++
@@ -1,3 +0,0 @@
-build/Punch-A-Rooney!/source/usefulfunctions.cpp.o: \
- source/usefulfunctions.cpp source/usefulfunctions.h
-source/usefulfunctions.h:
